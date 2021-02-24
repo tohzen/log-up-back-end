21f2efa1bbd807b2d27dc9a347d33e24ed66ac40
Tohzen <melvin.l.mobley@gmail.com>
2021-02-24 13:33:54 -0500

    finished

--- main.js
+++ main.js
@@ -14,9 +14,39 @@ const password3 = 'like a BOSS';
 // **YOUR** code below. Pass those tests!
 
 
+function isValidEmail(str) {
+    if (str.endsWith ('@codeimmersives.com') && str[0] !=="@" )
+ {
+    return true;
+  } else {
+    return false;
+  }
+  
+}
 
+function isValidPassword(str) {
+  if (str.length >= 8 && str.toUpperCase() !== str && str.toLowerCase() !== str){
+    return true;
+  }else{
+    return false;
+  }
+}
 
+function isRegisteredUser(str) {
+  if (str === user1 || str === user2 || str === user3) {
+    return true
+  } else {
+    return false
+  }
+}
 
+function passwordMatches(str1,str2) {
+  if (str1 === user1 && str2 === password1 || str1 === user2 && str2 === password2 || str1 === user3 && str2 === password3) {
+    return true
+  } else {
+    return false
+  }
+}
 
 // Our code below. Do not touch!
 
